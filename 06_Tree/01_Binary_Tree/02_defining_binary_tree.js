40bf57842854a95e84b79029de48b22f4a1f0355
Harsh Shahrma <sharmahs017@gmail.com>
2024-09-23 15:47:52 +0530

    add tree

---
+++ 06_Tree/01_Binary_Tree/02_defining_binary_tree.js
@@ -0,0 +1,18 @@
+// NOte it is just a basic exampe for defining
+
+class Node {
+    constructor(item) {
+        this.data = item;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+let firstNode = new Node(2);
+let secondNode = new Node(3);
+let thirdNode = new Node(4);
+let fourthNode = new Node(5);
+
+firstNode.left = secondNode;
+firstNode.right = thirdNode;
+secondNode.left = fourthNode;
